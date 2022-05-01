afa9cdee99c8e134eab3552396a59b66585e53d4
Gehmasse <leo@gehmasse.de>
2022-05-01 22:11:37 +0200

    removed obsolete indentions

--- index.js
+++ index.js
@@ -16,41 +16,41 @@ const attr_translations = {
 module.exports.getWordInformation = async function (word) {
     //send http request
     let res = await centra(`https://www.frag-caesar.de/lateinwoerterbuch/${word}-uebersetzung.html`).send();
-    
+
     //when more results exist
     if (res.body.toString().includes(`Ihr Suchwort <strong><span class="textmarker">${word}</span></strong> entspricht`))
         //redirect to first result
         res = await centra(`https://www.frag-caesar.de/lateinwoerterbuch/${word}-uebersetzung-1.html`).send();
-    
+
     //load content
     const $ = await cheerio.load(res.body.toString());
-    
+
     //put all table rows in array
     const tableElements = $('.table-responsive table tr').toArray();
-    
+
     //init counter and content for loop
     let i = 0;
     const content = {};
-    
+
     //for each tr
     tableElements.forEach(tr => {
         //increment counter
         i++;
-        
+
         //add array for current table row
         if (!content[i]) content[i] = [];
-        
+
         //for each td
         tr.children.forEach(td => {
             //init child content
             let childContent = '';
-            
+
             //for each td child as c (different translation of word)
             if (td.children) td.children.forEach(c => {
                 //add c to content string
                 if (c.data) childContent = childContent + `${c.data}|`;
             });
-            
+
             //push td content string to content array
             content[i].push(childContent);
         });
@@ -64,16 +64,16 @@ module.exports.getWordInformation = async function (word) {
 
     //init counter
     let y = 0;
-    
+
     //for each td content string from first tr as c
     content[1].forEach(c => {
         //get keys
         var key_returnForm = attr_translations[c.split('|').join('')] ? attr_translations[c.split('|').join('')] : c.split('|').join('');
         var key_content = content?.[3]?.[y] ? 3 : 2;
-        
+
         //get y-th translation
         returnForm[key_returnForm] = content[key_content][y].substring(0, content[key_content][y].length - 1);
-        
+
         //increment
         y++;
     });
