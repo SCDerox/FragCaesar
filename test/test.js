9d94ab48bed66190d16188e8728cdbc3a8b3ae3b
Gehmasse <leo@gehmasse.de>
2022-05-01 21:51:02 +0200

    improved test.js and text translation

--- test/test.js
+++ test/test.js
@@ -1,7 +1,15 @@
 const {getWordInformation} = require('../index');
 
-const sentence = 'Gallia est omnis divisa in partes tres, quarum unam incolunt Belgae, aliam Aquitani, tertiam qui ipsorum lingua Celtae, nostra Galli appellantur.'
+var sentence = 'Gallia est omnis divisa in partes tres, quarum unam incolunt Belgae, aliam Aquitani, tertiam qui ipsorum lingua Celtae, nostra Galli appellantur.'
 
-sentence.split(',').join('').split('.').join('').split(' ').forEach(async w => {
-    console.log(await getWordInformation(w) ? await getWordInformation(w) : w)
+var res = {}
+
+sentence = sentence.split(',').join('').split('.').join('').split(' ')
+
+var length = sentence.length
+
+sentence.forEach(async (w, key) => {
+    var wordInfo = await getWordInformation(w) ? await getWordInformation(w) : w
+    res[key] = wordInfo
+    if(Object.keys(res).length == length) await console.log(res)
 })
