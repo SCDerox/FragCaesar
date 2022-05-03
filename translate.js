a5812b86a40a66a4f3bb6c5489b144e7096ab946
Gehmasse <77433904+Gehmasse@users.noreply.github.com>
2022-05-03 20:18:08 +0200

    replaced var with let

--- translate.js
+++ translate.js
@@ -2,7 +2,7 @@
 const { getWordInformation, translate } = require('./index.js')
 
 //sentence
-var sentence = 'Gallia est omnis divisa in partes tres, quarum unam incolunt Belgae, aliam Aquitani, tertiam qui ipsorum lingua Celtae, nostra Galli appellantur.'
+const sentence = 'Gallia est omnis divisa in partes tres, quarum unam incolunt Belgae, aliam Aquitani, tertiam qui ipsorum lingua Celtae, nostra Galli appellantur.'
 
 //translate sentence and set callback for result array
 translate(sentence, res => console.log(res))
